c9633de4f3e44edb3e6836ee821a7f9c9d5b32c4
Abulude Michael <clevermike02@gmail.com>
2024-12-16 21:24:07 +0100

    models commit

--- main/models.py
+++ main/models.py
@@ -76,3 +76,11 @@ class BlogPost(models.Model):
 
     class Meta:
         ordering = ["-created_at"]
+
+
+class Expertise(models.Model):
+    title = models.CharField(max_length=50)
+    about = models.TextField(max_length=50)
+
+    def __str__(self):
+        return self.title
